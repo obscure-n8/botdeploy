4d17c99efd8961a991682a27551281bd54286df3
obscure <sheikharifinae4@gmail.com>
2026-09-18 18:46:45 +0600

    Update update.py

--- update.py
+++ update.py
@@ -40,7 +40,7 @@ if len(DATABASE_URL) == 0:
 
 if DATABASE_URL is not None:
     conn = MongoClient(DATABASE_URL)
-    db = conn.main
+    db = conn.kpsmlx
     old_config = db.settings.deployConfig.find_one({'_id': bot_id})
     config_dict = db.settings.config.find_one({'_id': bot_id})
     if old_config is not None:
